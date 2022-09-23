107639cb8913ef8358257627188038508e7d6f62
Aaron Rainbolt <arraybolt3@gmail.com>
2022-09-23 15:53:49 -0500

    Additional fixes (set kdedir to /usr, make passwordless sudo work)

--- KForgeInstaller.sh
+++ KForgeInstaller.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt -y full-upgrade
 sudo apt install -y git cmake dialog
-sudo echo "ALL            ALL = (ALL) NOPASSWD: ALL" > /etc/sudoers.d/nopwdsudo
+echo "ALL            ALL = (ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/nopwdsudo
 mkdir -p ~/kde/src
 cd ~/kde/src/
 git clone https://invent.kde.org/sdk/kdesrc-build.git && cd kdesrc-build
@@ -11,4 +11,7 @@ git clone https://invent.kde.org/sdk/kdesrc-build.git && cd kdesrc-build
 source ~/.bashrc
 sudo apt -y build-dep plasma-desktop plasma-workspace kwin phonon4qt5-backend-vlc phonon4qt5-backend-gstreamer breeze-gtk-theme kgamma5 xdg-desktop-portal-kde
 sudo apt -y install libqca-qt5-2-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev
+mv ~/.config/kdesrc-buildrc ~/.config/kdesrc-buildrc-bak
+sed 's|kdedir ~/kde/usr|kdedir /usr|' ~/.config/kdesrc-buildrc-bak > ~/.config/kdesrc-buildrc
+rm ~/.config/kdesrc-buildrc-bak
 reboot

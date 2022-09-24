a87fc1ff774bd755df5b36ce0a1459cff3ac3337
Aaron Rainbolt <arraybolt3@gmail.com>
2022-09-23 19:03:48 -0500

    Added downloader script

---
+++ KForgeDL.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-23/kforgeaa
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-23/kforgeab
+cat kforgeaa kforgeab > KFocus.ova
+rm kforgeaa kforgeab

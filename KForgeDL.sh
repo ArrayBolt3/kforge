849cf5c2ed536425896f78bd73321ce8d0a45e46
Aaron Rainbolt <arraybolt3@gmail.com>
2022-09-25 09:15:57 -0500

    Added SHA256 checking to the downloader

--- KForgeDL.sh
+++ KForgeDL.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-23/kforgeaa
 wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-23/kforgeab
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-23/SHA256SUMS
+sha256sum -C SHA256SUMS
+read -n 1 -s
 cat kforgeaa kforgeab > KForge.ova
 rm kforgeaa kforgeab

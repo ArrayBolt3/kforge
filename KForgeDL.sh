c3ab48351270e3c320c8d73e5a0fc8e5e43f473f
Aaron Rainbolt <arraybolt3@gmail.com>
2022-09-27 01:41:36 -0500

    Updated links in downloader script

--- KForgeDL.sh
+++ KForgeDL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-25/kforgeaa
-wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-25/kforgeab
-wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-25/SHA256SUMS
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-26/kforgeaa
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-26/kforgeab
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-9-26/SHA256SUMS
 sha256sum -C SHA256SUMS
 read -n 1 -s
 cat kforgeaa kforgeab > KForge.ova

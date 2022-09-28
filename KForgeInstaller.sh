8c7f58d0d1b97eeb79fb0075fdd16ee473b038a7
Aaron Rainbolt <arraybolt3@gmail.com>
2022-09-28 14:56:29 -0500

    Moved the libplymouth-dev line where it actually belongs

--- KForgeInstaller.sh
+++ KForgeInstaller.sh
@@ -9,8 +9,8 @@ cd ~/kde/src/
 git clone https://invent.kde.org/sdk/kdesrc-build.git && cd kdesrc-build
 ./kdesrc-build --initial-setup
 source ~/.bashrc
-sudo apt -y build-dep plasma-desktop plasma-workspace kwin phonon4qt5-backend-vlc phonon4qt5-backend-gstreamer breeze-gtk-theme kgamma5 xdg-desktop-portal-kde libplymouth-dev
-sudo apt -y install libqca-qt5-2-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev
+sudo apt -y build-dep plasma-desktop plasma-workspace kwin phonon4qt5-backend-vlc phonon4qt5-backend-gstreamer breeze-gtk-theme kgamma5 xdg-desktop-portal-kde
+sudo apt -y install libqca-qt5-2-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libplymouth-dev
 tee << EOF > ~/.config/kdesrc-buildrc
 # This file controls options to apply when configuring/building modules, and
 # controls which modules are built in the first place.

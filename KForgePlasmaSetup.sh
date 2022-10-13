88d83fdb9054ad28baf31f13d5b4d1aff96d2893
Aaron Rainbolt <arraybolt3@gmail.com>
2022-10-13 10:04:57 -0500

    Bug fixes in KForgePlasmaSetup

--- KForgePlasmaSetup.sh
+++ KForgePlasmaSetup.sh
@@ -4,7 +4,7 @@
 
 buildModScript="#!/bin/bash\ncd $1 && mkdir build && cmake -S . -B build && cd build ** make -j4 && sudo make install && cd ../.."
 buildAllScript="#!/bin/bash\nfor srcDir in `ls`; do ./buildMod $srcDir; done"
-repoList="bluedevil breeze breeze-grub breeze-gtk breeze-plymouth discover drkonqi kactivitymanagerd kde-cli-tools kdecoration kde-gtk-config kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit kscreen kscreenlocker ksshaskpass ksystemstats kwallet-pam kwayland-integration kwayland-server kwin kwrited layer-shell-qt libkscreen libksysguard milou oxygen plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-integration plasma-mobile plasma-nano plasma-nm plasma-pa plasma-sdk plasma-systemmonitor plasma-tests plasma-thunderbolt plasma-vault plasma-workspace plasma-workspace-wallpapers plymouth-kcm polkit-kde-agent powerdevil qqc2-breeze-style sddm-kcm systemsettings xdg-desktop-portal-kde"
+repoList="bluedevil breeze breeze-grub breeze-gtk breeze-plymouth discover drkonqi kactivitymanagerd kde-cli-tools kdecoration kde-gtk-config kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit kscreen kscreenlocker ksshaskpass ksystemstats kwallet-pam kwayland-integration kwayland-server kwin kwrited layer-shell-qt libkscreen libksysguard milou oxygen plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-integration plasma-mobile plasma-nano plasma-nm plasma-pa plasma-sdk plasma-systemmonitor plasma-tests plasma-thunderbolt plasma-vault plasma-workspace plasma-workspace-wallpapers plymouth-kcm polkit-kde-agent-1 powerdevil qqc2-breeze-style sddm-kcm systemsettings xdg-desktop-portal-kde"
 
 if [ -e "~/kplasma" ]; then
 	cd ~/kplasma
@@ -23,11 +23,9 @@ if [ -e "~/kplasma" ]; then
 else
 	mkdir ~/kplasma
 	cd ~/kplasma
-	for kRepo in #repoList; do
+	for kRepo in $repoList; do
 		git clone https://invent.kde.org/plasma/$kRepo.git
-		cd $kRepo
-		git checkout Plasma/$1
-		cd ..
+		cd $kRepo && git checkout Plasma/$1 && cd ..
 	done
 	echo -e $buildModScript | tee buildMod > /dev/null
 	echo -e $buildAllScript | tee buildAll > /dev/null

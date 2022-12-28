f2b90b2edcb62989f06156a9ecbb40efcba5f0a7
Aaron Rainbolt <arraybolt3@ubuntu.com>
2022-12-28 17:47:52 -0600

    Updated downloader and added SHA256SUM checking.

--- KForgeDL.sh
+++ KForgeDL.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-wget https://github.com/ArrayBolt3/kforge/releases/download/2022-10-13/kforgeaa
-wget https://github.com/ArrayBolt3/kforge/releases/download/2022-10-13/kforgeab
-cat kforgeaa kforgeab > KForge.ova
-rm kforgeaa kforgeab
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-12-28/kforgeaa
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-12-28/kforgeab
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-12-28/kforgeac
+wget https://github.com/ArrayBolt3/kforge/releases/download/2022-12-28/SHA256SUMS
+sha256sum -c SHA256SUMS
+if [ $? -eq 0 ]; then
+	cat kforgeaa kforgeab kforgeac > KForge.ova
+	rm kforgeaa kforgeab kforgeac
+else
+	echo "SHA256SUM error! Please run 'sha256sum -c SHA256SUMS' and redownload the damaged file(s)."
+fi
